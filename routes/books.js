cec025549449d5c526cf0f129bc0975b04704abb
Oluwasegun Adeleye <i.segunadeleye@gmail.com>
2017-12-22 15:41:21 +0100

    Fetch books

--- routes/books.js
+++ routes/books.js
@@ -3,9 +3,16 @@
 module.exports = app => {
   const Book = app.db.models.book;
 
-  app.post('/books', (req, res) => {
-    Book.create(req.body)
-      .then(book => res.json({ status: true, book }))
-      .catch(err => res.json({ status: false, err }));
-  });
+  app
+    .route('/books')
+    .post((req, res) => {
+      Book.create(req.body)
+        .then(book => res.json({ status: true, book }))
+        .catch(err => res.json({ status: false, err }));
+    })
+    .get((req, res) => {
+      Book.findAll({})
+        .then(book => res.json({ status: true, book }))
+        .catch(err => res.json({ status: false, err }));
+    });
 };
