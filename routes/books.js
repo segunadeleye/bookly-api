f88e5bb3d576b97ad84be32dd6864887bc6992d2
Oluwasegun Adeleye <i.segunadeleye@gmail.com>
2017-12-29 22:07:37 +0100

    apidoc Add documentation for /users and /books routes

--- routes/books.js
+++ routes/books.js
@@ -5,14 +5,113 @@ module.exports = app => {
 
   app
     .route('/books')
+    /**
+     * @api {post} /books Create Book
+     * @apiGroup Books
+     *
+     * @apiParam {String} title Book title
+     * @apiParam {String} author Book author
+     * @apiParam {String} edition Book edition
+     * @apiParam {Number} year Book year of release
+     * @apiParam {String} description Book description
+     * @apiParam {Number} user_id Id of user
+     * @apiParamExample {json} Input
+     *  {
+     *    "title": "Building APIs with Node.js",
+     *    "author": "Caio Ribeiro Pereira",
+     *    "edition": "1",
+     *    "year": 2016,
+     *    "description": "A book that takes you step by step on how to create production-ready APIs with Node.js",
+     *    "user_id": 1
+     *  }
+     *
+     * @apiSuccess {Boolean} status Response status
+     * @apiSuccess {Object} book Create book
+     * @apiSuccess {Number} book.id Book id
+     * @apiSuccess {String} book.title Book title
+     * @apiSuccess {String} book.author Book author
+     * @apiSuccess {String} book.edition Book edition
+     * @apiSuccess {Number} book.year Book year
+     * @apiSuccess {String} book.description Book description
+     * @apiSuccess {Number} book.user_id User Id
+     * @apiSuccess {Date} book.created_at Creation date
+     * @apiSuccess {Date} book.updated_at Update date
+     * @apiSuccessExample {json} Success
+     * HTTP/1.1 200 OK
+     *  {
+     *    success: true,
+     *    book: {
+     *      "id": 1,
+     *      "title": "Building APIs with Node.js",
+     *      "author": "Caio Ribeiro Pereira",
+     *      "edition": "1",
+     *      "year": 2016,
+     *      "description": "A book that takes you step by step on how to create production-ready APIs with Node.js"
+     *      "user_id": 1,
+     *      "created_at": "2017-12-22T13:45:56.000Z",
+     *      "updated_at": "2017-12-22T13:45:56.000Z"
+     *    }
+     *  }
+     *
+     * @apiError {Boolean} status Response Status
+     * @apiError {Object} err Response Error
+     * @apiErrorExample {json} Error
+     * HTTP/1.1 412 Precondition Failed
+     *  {
+     *    status: false,
+     *    err: {}
+     *  }
+     */
     .post((req, res) => {
       Book.create(req.body)
         .then(book => res.json({ status: true, book }))
-        .catch(err => res.json({ status: false, err }));
+        .catch(err => res.status(412).json({ status: false, err }));
     })
+
+    /**
+     * @api {get} /books List Books
+     * @apiGroup Books
+     *
+     * @apiSuccess {Boolean} status Response status
+     * @apiSuccess {Object[]} book Book list
+     * @apiSuccess {Number} book.id Book id
+     * @apiSuccess {String} book.title Book title
+     * @apiSuccess {String} book.author Book author
+     * @apiSuccess {String} book.edition Book edition
+     * @apiSuccess {Number} book.year Book year
+     * @apiSuccess {String} book.description Book description
+     * @apiSuccess {Number} book.user_id User Id
+     * @apiSuccess {Date} book.created_at Creation date
+     * @apiSuccess {Date} book.updated_at Updates date
+     * @apiSuccessExample {json} Success
+     * HTTP/1.1 200 OK
+     *  {
+     *    success: true,
+     *    book: [{
+     *      "id": 1,
+     *      "title": "Building APIs with Node.js",
+     *      "author": "Caio Ribeiro Pereira",
+     *      "edition": null,
+     *      "year": 2016,
+     *      "description": "A book that takes you step by step on how to create production-ready APIs with Node.js",
+     *      "created_at": "2017-12-22T13:45:56.000Z",
+     *      "updated_at": "2017-12-22T13:45:56.000Z",
+     *      "user_id": 1
+     *    }]
+     *  }
+     *
+     * @apiError {Boolean} status Response Status
+     * @apiError {Object} err Response Error
+     * @apiErrorExample {json} Error
+     * HTTP/1.1 412 Precondition Failed
+     *  {
+     *    status: false,
+     *    err: {}
+     *  }
+     */
     .get((req, res) => {
       Book.findAll({})
         .then(book => res.json({ status: true, book }))
-        .catch(err => res.json({ status: false, err }));
+        .catch(err => res.status(412).json({ status: false, err }));
     });
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,56 @@
+'use strict';
+
 module.exports = app => {
   const User = app.db.models.user;
 
+  /**
+   * @api {post} /users Create User
+   * @apiGroup Users
+   *
+   * @apiParam {String} name User's name
+   * @apiParam {String} email User's email
+   * @apiParam {String} username User's username
+   * @apiParam {String} password User's password
+   * @apiParamExample {json} Input
+   *  {
+   *    "name": "Oluwasegun Adeleye",
+   *    "email": "oluwasegun@bookly.com",
+   *    "username": "oluwasegun",
+   *    "password": "booklypass"
+   *  }
+   *
+   * @apiSuccess {Boolean} status Response status
+   * @apiSuccess {Object} user Created user
+   * @apiSuccess {Number} user.id User Id
+   * @apiSuccess {String} user.name User's name
+   * @apiSuccess {String} user.email User's email
+   * @apiSuccess {String} user.username User's username
+   * @apiSuccess {String} user.password User's password
+   * @apiSuccess {Date} user.created_at Creation date
+   * @apiSuccess {Date} user.updated_at Update date
+   * @apiSuccessExample {json} Success
+   * HTTP/1.1 200 OK
+   *  {
+   *    success: true,
+   *    user: {
+   *      "name": "Oluwasegun Adeleye",
+   *      "email": "oluwasegun@bookly.com",
+   *      "username": "oluwasegun",
+   *      "password": "sdeonnjfeaw23j4330nc",
+   *      "created_at": "2017-12-22T13:45:56.000Z",
+   *      "updated_at": "2017-12-22T13:45:56.000Z"
+   *    }
+   *  }
+   *
+   * @apiError {Boolean} status Response Status
+   * @apiError {Object} err Response Error
+   * @apiErrorExample {json} Error
+   * HTTP/1.1 412 Precondition Failed
+   *  {
+   *    status: false,
+   *    err: {}
+   *  }
+   */
   app.post('/users', (req, res) => {
     User.create(req.body)
       .then(user => res.json({ status: true, user }))
