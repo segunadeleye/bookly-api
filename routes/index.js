00432c23700e08142451db92ed6ac5e97104cef3
Oluwasegun Adeleye <i.segunadeleye@gmail.com>
2017-12-29 18:25:17 +0100

    apidoc: Setup apidoc for documentation. Write documentation for root url api

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,13 @@
 module.exports = app => {
+  /**
+   * @api {get} / API Status
+   * @apiGroup Status
+   * @apiSuccess {String} message API Status message
+   * @apiSuccessExample {json} Success
+   * HTTP/1.1 200 OK
+   * {message: "Bookly. Let's know what you are reading."}
+   *
+   */
   app.get('/', (req, res) => {
     res.json({ message: "Bookly. Let's know what you are reading." });
   });
